61196dfc23ddd882cfdefc1b86a44d89fb3e2163
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2025-08-27 22:23:20 -0700

    fix: update deprecated vim.validate fn & ensure vim.version is truthy

--- lua/inlay-hints/init.lua
+++ lua/inlay-hints/init.lua
@@ -1,16 +1,13 @@
-if not vim.fn.has("nvim-0.10") then
-  vim.notify("This plugin only works with neovim 0.10+, please update your neovim version", vim.log.levels.ERROR)
+if not vim.fn.has("nvim-0.11") == 1 then
+  vim.notify("This plugin only works with neovim 0.11+, please update your neovim version", vim.log.levels.ERROR)
   return
 end
 
 local M = {}
 
 M.setup = function(user_config)
-  vim.validate({ user_config = { user_config, "table", true } })
+  vim.validate("user_config", user_config, "table", true)
   require("inlay-hints.config").load(user_config)
 end
-
-M.on_attach = require("inlay-hints.utils").on_attach
-M.enable_inlay_hints_autocmd = require("inlay-hints.utils").enable_inlay_hints_autocmd
 
 return M
